b6ec21d1651a9766919b41c608ba0ef5f7ea3708
ujwal2003 <joshiujwal2003@gmail.com>
2024-05-29 17:48:02 -0500

    added navbar with icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
-<h1 class="text-white">
-    TEST
-</h1>
+<script lang="ts">
+    import { navIcons } from '$lib/icons/navIcons';
+    const NAV_MARGINS: any = {Home: 'mt-0.5', About: 'mb-1'}
+</script>
+
+<nav class='text-white text-2xl flex justify-between px-8 pt-8 pb-12'>
+    <div class="flex gap-4">
+        {#each Object.entries(navIcons) as [navItem, icon]}
+            <div class="flex gap-1">
+                {@html icon}
+                <span>{navItem}</span>
+            </div>
+        {/each}
+    </div>
+
+    <div>
+        <span>[in]</span>
+        <span>[gh]</span>
+    </div>
+</nav>
