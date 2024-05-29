a234f12b0c744651d932c084d5bb6ffb365deb1e
ujwal2003 <joshiujwal2003@gmail.com>
2024-05-29 18:07:13 -0500

    completed navbar, and added mobile responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
     import { navIcons } from '$lib/icons/navIcons';
-    const NAV_MARGINS: any = {Home: 'mt-0.5', About: 'mb-1'}
+    import NavLinkedInIco from '$lib/icons/navLinkedInIco.svelte';
+    import NavGitHubIco from '$lib/icons/navGitHubIco.svelte';
+
 </script>
 
-<nav class='text-white text-2xl flex justify-between px-8 pt-8 pb-12'>
-    <div class="flex gap-4">
+<nav class='text-white text-2xl flex justify-center md:justify-between px-8 pt-8 pb-12 fixed bottom-0 left-0 w-full md:static z-50 bg-gray-900'>
+    <div class="flex gap-10 md:gap-8">
         {#each Object.entries(navIcons) as [navItem, icon]}
-            <div class="flex gap-1">
+            <div class="flex gap-2">
                 {@html icon}
-                <span>{navItem}</span>
+                <span class="hidden md:block">{navItem}</span>
             </div>
         {/each}
     </div>
 
-    <div>
-        <span>[in]</span>
-        <span>[gh]</span>
+    <div class="hidden md:block">
+        <div class="flex gap-4 pt-1">
+            <NavLinkedInIco />
+            <NavGitHubIco />
+        </div>
     </div>
 </nav>
