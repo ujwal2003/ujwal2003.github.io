613b865d38e415ddae3f4d0137a5720d2e27d17b
ujwal2003 <joshiujwal2003@gmail.com>
2024-05-29 19:31:53 -0500

    moved navigation bar to bottom of mobile view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,25 @@
 
 </script>
 
-<nav class='text-white text-2xl flex justify-center md:justify-between px-8 pt-8 pb-12 fixed bottom-0 left-0 w-full md:static z-50 bg-gray-900'>
-    <div class="flex gap-10 md:gap-8">
+<nav class='text-white text-2xl flex justify-center md:justify-between px-8 pt-8 pb-12 fixed bottom-0 left-0 w-full md:static z-50 
+            max-sm:bg-gray-900 max-sm:border max-sm:border-slate-800 max-sm:rounded-2xl max-sm:pb-6 max-sm:mb-4'>
+    <div class="flex gap-10 max-sm:gap-8">
         {#each Object.entries(navIcons) as [navItem, icon]}
-            <div class="flex gap-2">
+            <div class="flex gap-2 cursor-pointer hover:scale-110">
                 {@html icon}
                 <span class="hidden md:block">{navItem}</span>
             </div>
         {/each}
     </div>
 
-    <div class="hidden md:block">
-        <div class="flex gap-4 pt-1">
-            <NavLinkedInIco />
-            <NavGitHubIco />
+    <div class="hidden lg:block">
+        <div class="flex gap-4 pt-2 cursor-pointer">
+            <span class="hover:scale-110">
+                <NavLinkedInIco />
+            </span>
+            <span class="hover:scale-110">
+                <NavGitHubIco />
+            </span>
         </div>
     </div>
 </nav>
