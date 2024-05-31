cfb1cd0f22515f33e3ed234ddad18dfdcbc3c273
ujwal2003 <joshiujwal2003@gmail.com>
2024-05-31 13:21:41 -0500

    mobile responsiveness to about me section

--- src/lib/Heading.svelte
+++ src/lib/Heading.svelte
@@ -1,9 +1,11 @@
-<div class="text-[#768087] text-3xl font-sans pb-2 max-sm:text-center">
+<div class="text-[#768087] text-3xl font-sans pb-2">
     Hi! My name is
 </div>
-<div class="text-white text-8xl font-sans font-extrabold pb-6 max-sm:text-center max-sm:text-6xl">
+
+<div class="text-white text-8xl font-sans font-extrabold pb-6 max-sm:text-6xl">
     Ujwal Joshi,
 </div>
-<div class="text-white text-5xl font-sans max-sm:text-2xl max-sm:text-center">
+
+<div class="text-white text-5xl font-sans max-sm:text-2xl">
     an aspiring Software Engineer.
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,22 +31,16 @@
     </div>
 </nav>
 
-<!-- <div class="flex justify-center pt-16"> -->
-    <!-- max-w-screen-md -->
-    <!-- <div class="flex flex-col items-center"> -->
-        <div class="flex flex-col ml-64 pt-16">
-            <Heading />
-            <HeroConnections />
-    
-            <!-- <div class="w-2/3"> -->
-                <h1 class="text-white font-semibold text-5xl mt-24 mr-auto">
-                    About Me
-                </h1>
-                <p class="text-white text-wrap text-2xl pr-36 mx-auto mt-6 leading-snug">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            <!-- </div> -->
-        </div>
+<div class="flex flex-col ml-64 pt-16 max-sm:ml-8">
+    <Heading />
+    <HeroConnections />
 
-    <!-- </div> -->
-<!-- </div> -->
+    <h1 class="text-white font-semibold text-5xl mt-24 mr-auto max-sm:text-3xl max-sm:mt-14">
+        About Me
+    </h1>
+
+    <p class="text-white text-wrap text-2xl pr-36 mx-auto mt-6 leading-snug 
+                max-sm:pr-3 max-sm:text-xl max-sm:mt-4 max-sm:pb-28">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</div>
