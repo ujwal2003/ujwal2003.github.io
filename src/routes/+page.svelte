4c8087e77404d18b988fb58220c0a748a97cd3b8
ujwal2003 <joshiujwal2003@gmail.com>
2024-05-30 19:14:05 -0500

    nav icons scale on mobile when active

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,10 @@
 
 <nav class='text-white text-2xl flex justify-center md:justify-between px-8 pt-8 pb-12 fixed bottom-0 left-0 w-full md:static z-50 
             max-sm:bg-gray-900 max-sm:border max-sm:border-slate-800 max-sm:rounded-2xl max-sm:pb-6 max-sm:mb-4'>
+
     <div class="flex gap-10 max-sm:gap-8">
         {#each Object.entries(navIcons) as [navItem, icon]}
-            <div class="flex gap-2 cursor-pointer hover:scale-110">
+            <div class="flex gap-2 cursor-pointer md:hover:scale-110 max-sm:active:scale-110">
                 {@html icon}
                 <span class="hidden md:block">{navItem}</span>
             </div>
