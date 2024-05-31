fdfb1b01fed98984b58f025dff30390308e9072a
ujwal2003 <joshiujwal2003@gmail.com>
2024-05-30 20:28:02 -0500

    started about me section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,5 +35,14 @@
     <div class="flex-col">
         <Heading />
         <HeroConnections />
+
+        <div class="w-1/2">
+            <h1 class="text-white font-semibold text-5xl mt-24">
+                About Me
+            </h1>
+            <!-- <p class="text-white text-wrap text-2xl w-1/2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p> -->
+        </div>
     </div>
 </div>
