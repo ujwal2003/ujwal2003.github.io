7edf02682ac3fb66ab31aa2a9b7af7f263f0eab6
ujwal2003 <joshiujwal2003@gmail.com>
2024-06-01 16:35:37 -0500

    description content desktop

--- src/lib/MainPage/Experience.svelte
+++ src/lib/MainPage/Experience.svelte
@@ -10,7 +10,44 @@
     </div>
 </h1>
 
-<p class="text-white text-wrap text-2xl pr-44 mx-auto mt-6 leading-relaxed 
+<!-- <p class="text-white text-wrap text-2xl pr-44 mx-auto mt-6 leading-relaxed 
             max-sm:pr-3 max-sm:text-xl max-sm:mt-4 max-sm:pb-12 max-sm:leading-7">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</p>
+</p> -->
+
+<div class="mt-6">
+
+    <div class="flex gap-24">
+        <div class="pt-0.5">
+            <h2 class="text-white text-3xl font-medium">
+                Boeing
+            </h2>
+    
+            <h3 class="text-[#768087] text-lg mt-2">
+                Jun. 2024 - Present
+            </h3>
+        </div>
+
+        <div>
+            <h4 class="text-white text-[28px]">
+                Digital Aviations Solutions Intern
+            </h4>
+
+            <p class="text-[#7F94A6] text-xl w-2/3 mt-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+
+            <div class="mt-3">
+                <button type="button" class="py-1 px-2 inline-flex items-center gap-x-2 text-lg rounded-lg border border-[#7F94A6] text-[#7F94A6] disabled:opacity-50 
+                                             disabled:pointer-events-none hover:bg-slate-300/20">
+                    Learn More 
+
+                    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.46671 21L5.83337 19.3667L17.0334 8.16666H7.00004V5.83333H21V19.8333H18.6667V9.79999L7.46671 21Z" fill="#7F94A6"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+
+</div>
