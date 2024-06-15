0fd2353db34251193f225a11017d35dc6e408ae4
ujwal2003 <joshiujwal2003@gmail.com>
2024-06-15 18:52:35 -0500

    added mobile responsiveness to project section component mock

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,26 +6,29 @@
 
 <NavBar NavSections={{Home: '/', About: "/#AboutMe", Experience: "/#ExprSection", Projects: "/#ProjSection", Contact: "/#ContactMe"}} smoothScroll={false} />
 
-<div class="flex flex-col ml-44 mt-6">
-    <h1 class="text-white font-semibold text-[64px]">
+<div class="flex flex-col ml-44 mt-6 max-sm:ml-0">
+    <h1 class="text-white font-semibold text-[64px] max-sm:text-[40px] max-sm:text-center">
         Personal Projects
     </h1>
 
     <!-- project section -->
-    <section class="w-5/6 mt-2">
-        <hr class="border-gray-500">
-        <div class="mt-5 flex gap-24">
-            <div class="flex flex-col gap-4">
-                <h2 class="text-white font-medium text-5xl">
-                    Chat AIr
-                </h2>
-
-                <h3 class="text-[#768087] text-xl pl-2">
-                    February 2024
-                </h3>
+    <section class="w-5/6 mt-2 max-sm:w-full max-sm:mt-4">
+        <hr class="border-gray-500 max-sm:mx-7">
+        <div class="mt-5 flex gap-24 max-sm:flex-col max-sm:gap-6">
+            
+            <div class="flex flex-col gap-4 max-sm:ml-11">
+                <div class="flex flex-col gap-4 max-sm:flex-row max-sm:gap-8">
+                    <h2 class="text-white font-medium text-5xl max-sm:text-[32px]">
+                        Chat AIr
+                    </h2>
+    
+                    <h3 class="text-[#768087] text-xl pl-2 max-sm:text-base">
+                        February 2024
+                    </h3>
+                </div>
                 
-                <div class="flex flex-col gap-2 pl-2">
-                    <h4 class="text-[#768087] text-xl">
+                <div class="flex flex-col gap-2 pl-2 max-sm:flex-row max-sm:gap-[3.8rem] max-sm:pl-0">
+                    <h4 class="text-[#768087] text-xl max-sm:text-base">
                         Technologies:
                     </h4>
 
@@ -36,25 +39,25 @@
                         <code><img width="42" src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png" alt="Figma" title="Figma"/></code>
                     </div>
     
-                    <p class="text-[#7F94A6] text-sm w-64">
+                    <p class="text-[#7F94A6] text-sm w-64 max-sm:w-56">
                         React Native, ExpressJS, RapidAPI, Amadeus, CompromiseJS
                     </p>
                 </div>
             </div>
 
-            <div>
-                <p class="text-[#7F94A6] text-2xl">
+            <div class="max-sm:ml-11">
+                <p class="text-[#7F94A6] text-2xl max-sm:text-lg">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
 
                 <div class="flex gap-4 mt-6">
                     <LearnMoreButton disabled />
-                    <ContentButton arrowIcon smallText noTopMargin on:contentClick={() => window.open('https://github.com/ujwal2003/CodeRED2024', '_blank')}>
+                    <ContentButton arrowIcon smallText noTopMargin on:contentClick={() => window.open('https://github.com/ujwal2003', '_blank')}>
                         GitHub Repo
                     </ContentButton>
                 </div>
-
             </div>
+
         </div>
     </section>
 
