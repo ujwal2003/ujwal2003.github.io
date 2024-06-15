b2de0a724190885758fcf681b7a925c0aed1ba3f
ujwal2003 <joshiujwal2003@gmail.com>
2024-06-15 18:54:01 -0500

    added href property to highlight link component

--- src/lib/components/highlightLink.svelte
+++ src/lib/components/highlightLink.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+    export let href: string;
+</script>
+
 <a  class="hover:bg-none hover:underline hover:decoration-sky-500/[.25] hover:underline-offset-4 hover:decoration-2 hover:decoration-double"
-    href="https://codered-genesis.devpost.com/" target="_blank">
+    href={href} target="_blank">
     <span class="bg-sky-500/[.05]">
         <slot></slot>
     </span>
