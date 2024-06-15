fbe82c6776cf18919d5dfd1dfadbe709e94601a2
ujwal2003 <joshiujwal2003@gmail.com>
2024-06-15 18:52:50 -0500

    highlighted link component

---
+++ src/lib/components/highlightLink.svelte
@@ -0,0 +1,6 @@
+<a  class="hover:bg-none hover:underline hover:decoration-sky-500/[.25] hover:underline-offset-4 hover:decoration-2 hover:decoration-double"
+    href="https://codered-genesis.devpost.com/" target="_blank">
+    <span class="bg-sky-500/[.05]">
+        <slot></slot>
+    </span>
+</a>
