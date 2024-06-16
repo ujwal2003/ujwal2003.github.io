3f379668400c0e2207191637ebb8a8b8ef8aad2c
ujwal2003 <joshiujwal2003@gmail.com>
2024-06-15 19:10:18 -0500

    adjusted height padding for project card buttons

--- src/lib/components/ProjectCard/CardButtons/learnMoreButton.svelte
+++ src/lib/components/ProjectCard/CardButtons/learnMoreButton.svelte
@@ -7,12 +7,12 @@
 
 {#if disabled}
     <button class="py-1.5 px-8 inline-flex items-center gap-x-2 text-md rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 
-                disabled:opacity-50  disabled:cursor-no-drop disabled:hover:bg-blue-600 md:mb-2" disabled type="button">
+                   disabled:opacity-50 disabled:cursor-no-drop disabled:hover:bg-blue-600 md:mb-2 max-sm:py-1" disabled type="button">
         Learn More
     </button>
 {:else}
     <button class="py-1.5 px-8 inline-flex items-center gap-x-2 text-md rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 
-                   disabled:opacity-50  disabled:cursor-no-drop disabled:hover:bg-blue-600 md:mb-2" type="button" on:click={() => dispatch('learnMoreClick')}>
+                   disabled:opacity-50 disabled:cursor-no-drop disabled:hover:bg-blue-600 md:mb-2 max-sm:py-1" type="button" on:click={() => dispatch('learnMoreClick')}>
     Learn More
     </button>
 {/if}
