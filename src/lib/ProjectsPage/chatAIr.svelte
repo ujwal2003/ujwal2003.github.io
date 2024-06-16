e2c09345d8075506d0979b048f0b08fe32386a5e
ujwal2003 <joshiujwal2003@gmail.com>
2024-06-16 18:33:14 -0500

    chat air project section

---
+++ src/lib/ProjectsPage/chatAIr.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import ProjectSection from "$lib/components/ProjectSection/projectSection.svelte";
+    import ProjectHeader from "$lib/components/ProjectSection/ProjectSectionHead/projectHeader.svelte";
+
+    import ProjectTitleArea from "$lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projectTitleArea.svelte";
+    import ProjTitleText from "$lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte";
+    import ProjectDate from "$lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projectDate.svelte";
+
+    import ProjectTechArea from "$lib/components/ProjectSection/ProjectSectionHead/ProjectTechnologies/projectTechArea.svelte";
+    import TechIconsArea from "$lib/components/ProjectSection/ProjectSectionHead/ProjectTechnologies/techIconsArea.svelte";
+    import ProjTechText from "$lib/components/ProjectSection/ProjectSectionHead/ProjectTechnologies/projTechText.svelte";
+
+    import ProjectDescriptionArea from "$lib/components/ProjectSection/ProjectDescription/projectDescriptionArea.svelte";
+    import ProjDescriptionText from "$lib/components/ProjectSection/ProjectDescription/projDescriptionText.svelte";
+    import ProjectButtonArea from "$lib/components/ProjectSection/ProjectDescription/projectButtonArea.svelte";
+    import ContentButton from "$lib/components/DescriptionContent/contentButton.svelte";
+    import LearnMoreButton from "$lib/components/ProjectCard/CardButtons/learnMoreButton.svelte";
+    import HighlightLink from "$lib/components/highlightLink.svelte";
+</script>
+
+<ProjectSection>
+    <ProjectHeader>
+        <ProjectTitleArea>
+            <ProjTitleText> Chat AIr </ProjTitleText>
+            <ProjectDate> February 2024 </ProjectDate>
+        </ProjectTitleArea>
+
+        <ProjectTechArea>
+            <TechIconsArea>
+                <!-- https://marwin1991.github.io/profile-technology-icons/ -->
+                <code><img width="42" src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/></code>
+                <code><img width="42" src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/></code>
+                <code><img width="42" src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png" alt="Express" title="Express"/></code>
+                <code><img width="42" src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png" alt="Figma" title="Figma"/></code>
+            </TechIconsArea>
+
+            <ProjTechText>
+                React Native, ExpressJS, RapidAPI, Amadeus, CompromiseJS
+            </ProjTechText>
+        </ProjectTechArea>
+    </ProjectHeader>
+
+    <ProjectDescriptionArea>
+        <ProjDescriptionText>
+            Mobile chatbot that uses natural language processing user asks the bot for the cheapest flights between two destinations in 
+            casual 'human like' conversation. Bot returns cheapest flights and all airports in the journey. 
+            First place winner at the 
+            <HighlightLink href="https://codered-genesis.devpost.com/">CodeRED: Genisis Hackathon</HighlightLink> 
+            PROS track.
+        </ProjDescriptionText>
+
+        <ProjectButtonArea>
+            <LearnMoreButton on:learnMoreClick={() => window.open('https://devpost.com/software/chatair', '_blank')} />
+            <ContentButton arrowIcon smallText noTopMargin on:contentClick={() => window.open('https://github.com/ujwal2003/CodeRED2024', '_blank')}>
+                GitHub Repo
+            </ContentButton>
+        </ProjectButtonArea>
+    </ProjectDescriptionArea>
+</ProjectSection>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import NavBar from "$lib/NavBar.svelte";
-    import ContentButton from "$lib/components/DescriptionContent/contentButton.svelte";
-    import LearnMoreButton from "$lib/components/ProjectCard/CardButtons/learnMoreButton.svelte";
+    import ChatAIr from "$lib/ProjectsPage/chatAIr.svelte";
 </script>
 
 <NavBar NavSections={{Home: '/', About: "/#AboutMe", Experience: "/#ExprSection", Projects: "/#ProjSection", Contact: "/#ContactMe"}} smoothScroll={false} />
@@ -11,52 +10,5 @@
         Personal Projects
     </h1>
 
-    <!-- <section class="w-5/6 mt-2 max-sm:w-full max-sm:mt-4">
-        <hr class="border-gray-500 max-sm:mx-7">
-
-        <div class="mt-5 flex gap-24 max-sm:flex-col max-sm:gap-6">
-            <div class="flex flex-col gap-4 max-sm:items-center">
-                <div class="flex flex-col gap-4 max-sm:flex-row max-sm:gap-16">
-                    <h2 class="text-white font-medium text-5xl max-sm:text-[32px]">
-                        Chat AIr
-                    </h2>
-    
-                    <h3 class="text-[#768087] text-xl pl-2 max-sm:text-base">
-                        February 2024
-                    </h3>
-                </div>
-                
-                <div class="flex flex-col gap-2 pl-2 max-sm:pl-0 max-sm:pr-12">
-                    <h4 class="text-[#768087] text-xl max-sm:text-base">
-                        Technologies:
-                    </h4>
-
-                    <div class="flex gap-1 max-sm:hidden">
-                        <code><img width="42" src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" alt="JavaScript" title="JavaScript"/></code>
-                        <code><img width="42" src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" alt="React" title="React"/></code>
-                        <code><img width="42" src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png" alt="Express" title="Express"/></code>
-                        <code><img width="42" src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png" alt="Figma" title="Figma"/></code>
-                    </div>
-    
-                    <p class="text-[#7F94A6] text-sm w-64 max-sm:pl-0.5">
-                        React Native, ExpressJS, RapidAPI, Amadeus, CompromiseJS
-                    </p>
-                </div>
-            </div>
-
-            <div class="max-sm:mx-10">
-                <p class="text-[#7F94A6] text-2xl max-sm:text-lg">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-
-                <div class="flex gap-4 mt-6">
-                    <LearnMoreButton disabled />
-                    <ContentButton arrowIcon smallText noTopMargin on:contentClick={() => window.open('https://github.com/ujwal2003', '_blank')}>
-                        GitHub Repo
-                    </ContentButton>
-                </div>
-            </div>
-        </div>
-    </section> -->
-
+    <ChatAIr />
 </div>
