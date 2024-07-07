d678efe7444e87c6f9d7dc1939f855dfaa0b77af
ujwal2003 <joshiujwal2003@gmail.com>
2024-07-07 17:04:14 -0500

    style props for custom font size for title text

--- src/lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte
+++ src/lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte
@@ -1,8 +1,15 @@
-<script lang="ts">
-    export let mobileFontSize: string = "32px";
-    export let defaultFontSize: string = "3rem";
-</script>
+<style>
+    .titleFontSize {
+        font-size: var(--pcFontSize, 3rem);
+    }
 
-<h2 class={`text-white font-medium text-[${defaultFontSize}] leading-none max-sm:text-[${mobileFontSize}] w-60 max-sm:w-32`}>
+    @media not all and (min-width: 640px) {
+        .titleFontSize {
+            font-size: var(--mobileFontSize, 32px);
+        }
+    }
+</style>
+
+<h2 class="text-white font-medium leading-none w-60 max-sm:w-32 titleFontSize">
     <slot></slot>
 </h2>
