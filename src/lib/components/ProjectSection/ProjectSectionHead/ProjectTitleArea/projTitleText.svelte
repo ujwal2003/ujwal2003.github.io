35d7951ebfda1f50a1023fcdfde2eb0f9365daa0
ujwal2003 <joshiujwal2003@gmail.com>
2024-07-07 16:35:08 -0500

    project title font size custom property

--- src/lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte
+++ src/lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte
@@ -1,3 +1,7 @@
-<h2 class="text-white font-medium text-5xl max-sm:text-[32px] w-60 max-sm:w-32">
+<script lang="ts">
+    export let fontSize: string = "32px";
+</script>
+
+<h2 class={`text-white font-medium text-5xl max-sm:text-[${fontSize}] w-60 max-sm:w-32`}>
     <slot></slot>
 </h2>
