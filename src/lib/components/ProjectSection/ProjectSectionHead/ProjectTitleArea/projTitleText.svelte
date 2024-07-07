9dbb51086eeef0a9de398d4aaf431b8cec0216cb
ujwal2003 <joshiujwal2003@gmail.com>
2024-07-07 16:48:29 -0500

    fixed project title custom font configuration

--- src/lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte
+++ src/lib/components/ProjectSection/ProjectSectionHead/ProjectTitleArea/projTitleText.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    export let fontSize: string = "32px";
+    export let mobileFontSize: string = "32px";
+    export let defaultFontSize: string = "3rem";
 </script>
 
-<h2 class={`text-white font-medium text-5xl max-sm:text-[${fontSize}] w-60 max-sm:w-32`}>
+<h2 class={`text-white font-medium text-[${defaultFontSize}] leading-none max-sm:text-[${mobileFontSize}] w-60 max-sm:w-32`}>
     <slot></slot>
 </h2>
