dd20ce8b66f87b3f1afa84f8a217fb67ce94b0f8
ujwal2003 <joshiujwal2003@gmail.com>
2025-01-21 18:29:20 -0600

    filled out experience section

--- src/lib/MainPage/Experience.svelte
+++ src/lib/MainPage/Experience.svelte
@@ -3,6 +3,7 @@
     import DescriptionContainerGrid from "$lib/components/DescriptionCard/DescriptionContainerGrid.svelte";
     import DescriptionCardTitle from "$lib/components/DescriptionCard/DescriptionCardTitle.svelte";
     import DescriptionCardContent from "$lib/components/DescriptionCard/DescriptionCardContent.svelte";
+    import DescriptionContent from "$lib/components/DescriptionContent/descriptionContent.svelte";
 </script>
 
 <h1 class="flex text-white font-semibold text-5xl mt-24 max-sm:text-4xl max-sm:mt-0 pt-4" id="ExprSection">
@@ -20,13 +21,35 @@
 
 <DescriptionContainerGrid>
     <DescriptionCardTitle 
-        company={'Company'} 
-        duration={'Month 20XX - Present'} 
+        company={'Paycom'}
+        duration={'Dec. 2024 - Present'}
     />
 
-    <DescriptionCardContent jobTitle={'Job Title'}>
+    <DescriptionCardContent jobTitle={'Software Developer'}>
         {#snippet jobDescription()}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            I am currently working at Paycom as a Software Developer.
+        {/snippet}
+    </DescriptionCardContent>
+
+    <DescriptionCardTitle 
+        company={'Boeing'}
+        duration={'Jun. 2024 - Aug. 2024'}
+    />
+
+    <DescriptionCardContent jobTitle={'Digital Aviation Solutions - Development Intern'}>
+        {#snippet jobDescription()}
+            Collaborated with a 10-member team of Software and DevOps engineers to enhance navigation data analysis and flight message validation processes. Automated data analysis with shell scripts and improved data readability and accuracy using Python. Integrated validation tools into a CI/CD pipeline with Docker, streamlining deployment workflows and providing clear, user-friendly test result displays.
+        {/snippet}
+    </DescriptionCardContent>
+
+    <DescriptionCardTitle 
+        company={'CougarCS'} 
+        duration={'Aug. 2023 - Aug. 2024'} 
+    />
+
+    <DescriptionCardContent jobTitle={'Computer Science Tutor'}>
+        {#snippet jobDescription()}
+            Guided over 11 students weekly on coding assignments, projects, and lab exercises, while also conducting one-on-one and group tutoring sessions in Computer Science courses such as Data Structures. Collaborated with a team of 30 tutors to create customized lesson plans, resulting in a 12% average increase in student exam scores.
         {/snippet}
     </DescriptionCardContent>
 </DescriptionContainerGrid>
